12e6651ecf625dc20fe3870efbe490b0618d0294
basst314 <sebastian.schlote@gmail.com>
2016-08-18 09:59:21 +0200

    initial commit

---
+++ src/main/java/com/space/SpaceFantasyWorld.java
@@ -0,0 +1,33 @@
+package com.space;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SpaceFantasyWorld implements SpaceWorldProvider {
+
+	private static final double EVENT_PROP = 0.9;
+	
+	private static final List<String> EVENTS = Arrays.asList("R", "T", "M", "W");
+	
+	@Override
+	public String getWorldConent() {
+		double rand = Math.random();
+		if (rand > EVENT_PROP){
+			int eventNo = (int)(Math.random() * EVENTS.size());
+			return EVENTS.get(eventNo);
+		} else {
+			return ".";
+		}
+	}
+
+	@Override
+	public String interact() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public String getHero(){
+		return "H";
+	}
+
+}
