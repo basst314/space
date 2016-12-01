8e3f00f51c8858ac87a2ed260d6e4ef6d5efe943
superernie77 <space77>
2016-12-01 19:52:18 +0100

    API for Space DAOs

---
+++ space-server/src/main/java/com/space/server/dao/api/WorldDao.java
@@ -0,0 +1,18 @@
+package com.space.server.dao.api;
+
+import java.util.List;
+
+/**
+ * Service for access to Space-Worlds.
+ * Created by superernie77 on 01.12.2016.
+ */
+public interface WorldDao {
+
+    void saveWorld(SpaceWorld world);
+
+    SpaceWorld loadWorld(int worldId);
+
+    List<SpaceWorld> getWorldsByPlayerId(int playerId);
+
+    List<SpaceWorld> getWorlds();
+}
