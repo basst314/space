1c47b7bc6d10e2b1aefd09690340ff1096ebe5f5
superernie77 <space77>
2016-12-01 20:03:14 +0100

    API for Space DAOs

---
+++ space-server/src/main/java/com/space/server/game/api/GameEngine.java
@@ -0,0 +1,28 @@
+package com.space.server.game.api;
+
+import com.space.server.dao.api.SpacePlayer;
+import com.space.server.dao.api.SpaceWorld;
+import com.space.server.engine.api.WorldEvent;
+
+/**
+ * Interface for space game manager. Stores and calculates the world state from the
+ * active players in the world and any submited world events.
+ * Created by superernie77 on 01.12.2016.
+ */
+public interface GameEngine {
+
+
+    void startPlayer(int playerId, int worldId);
+
+    void stoPlayer(int playerId, int worldId);
+
+    void stepWorld(int worldId);
+
+    void addEvent(WorldEvent event , int playerId);
+
+    SpaceWorld getWorld(int playerId);
+
+    void persist(int worldId);
+
+    SpacePlayer getPlayer(int playerId);
+}
