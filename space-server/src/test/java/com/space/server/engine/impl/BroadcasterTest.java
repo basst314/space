11206a405c430e81f923434915854103e4c0b992
superernie77 <space77>
2017-02-11 10:58:55 +0100

    Broadcaster test extended

--- space-server/src/test/java/com/space/server/engine/impl/BroadcasterTest.java
+++ space-server/src/test/java/com/space/server/engine/impl/BroadcasterTest.java
@@ -1,7 +1,15 @@
 package com.space.server.engine.impl;
 
+import static org.mockito.Mockito.*;
+
+import com.space.server.domain.api.Segment;
+import com.space.server.domain.api.SpaceWorld;
+import com.space.server.domain.impl.SimpleWorldImpl;
 import com.space.server.engine.api.GameEngine;
+import com.space.server.engine.api.WorldEvent;
+import com.space.server.engine.api.WorldEventType;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -10,15 +18,26 @@ import org.mockito.Mockito;
  */
 public class BroadcasterTest {
 
-    @Test
-    public void testBroadCaster(){
-        Broadcaster broadcaster = new Broadcaster();
+    private Broadcaster broadcaster;
 
-        broadcaster.setEngine(Mockito.mock(GameEngine.class));
+    GameEngine engine;
+
+    @Before
+    public void setup(){
+        broadcaster = new Broadcaster();
+
+        engine = Mockito.mock(GameEngine.class);
+
+        broadcaster.setEngine(engine);
 
         broadcaster.setPlayerId(1);
 
         broadcaster.setWorldId(0);
+
+    }
+
+    @Test
+    public void testBroadCaster(){
 
         Assert.assertTrue(broadcaster.getPlayerId() == 1);
 
@@ -26,4 +45,26 @@ public class BroadcasterTest {
 
         Assert.assertTrue(broadcaster.getEngine() != null);
     }
+
+    @Test
+    public void testCreateWorldEvent(){
+
+        SpaceWorld world = mock(SpaceWorld.class);
+
+        when(world.getSegment(0)).thenReturn(mock(Segment.class));
+
+        when(engine.getWorld(anyInt())).thenReturn(world);
+
+        WorldEvent event = broadcaster.createWorldEvent();
+
+        Assert.assertNotNull(event);
+
+        Assert.assertTrue(event.getPlayerId() == 1);
+        Assert.assertTrue(event.getWorldId() == 0);
+        Assert.assertTrue(event.getWorld() != null);
+        Assert.assertTrue(event.getType() == WorldEventType.UPDATE);
+
+    }
+
+
 }
