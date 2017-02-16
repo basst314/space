162df0f9aad7f997775c7f1f4fba02b1c82b6db6
superernie77 <space77>
2017-02-16 18:13:38 +0100

    Updated DB scripts

--- space-server/src/main/resources/sql/insert-data.sql
+++ space-server/src/main/resources/sql/insert-data.sql
@@ -1,3 +1,7 @@
 INSERT INTO PLAYER VALUES (0);
+INSERT INTO PLAYER VALUES (1);
 
-INSERT INTO SPACE_WORLD VALUES (0,0,0,'.......W.......M...M')
+INSERT INTO SPACE_WORLD VALUES (0,0,0)
+
+insert into segment values (0 ,0 , 0, '.......W.......M...M' )
+insert into segment values (0 ,1 , 1, '....................' )
